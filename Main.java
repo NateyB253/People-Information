b90e952e1b56a6a0062008421b9a0451da5fd575
NateyB253 <nater778@live.com>
2018-08-13 17:30:45 -0700

    Created Stream method but running into some errors

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+import java.nio.InvalidMarkException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.io.Paths;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
 
@@ -8,31 +14,8 @@ public class Main {
 		int input = kb.nextInt();
 		
 		Person[] people = new Person[25];
-		people[0] = new Person("Dustin","Cobb", 61,"Pepkor Park", "Zuhhotseb", "NM", "37990");
-		people[1] = new Person("Allen", "Greer",20,"Rinem Plaza", "Ozenanep", "TN", "98973");
-		people[2] = new Person("Lucile","West",33,"Pavo Trail","Sucdennec","MN","24853");
-		people[3] = new Person("Carolyn","Rivera",58,"Hizcah Point","Rarhucta","SC","96055");
-		people[4] = new Person("Ruby","Walsh",21,"Jeve Glen","Hicauze","IN","99661");
-		people[5] = new Person("Ricky","Bridges",54,"Hafic Point","Begebvi","WV","83957");
-		people[6] = new Person("Lula","Webb",21,"Uwose Drive","Seicuip","ID","77608");
-		people[7] = new Person("Don","Franklin",46,"Rigaw Mill","Nebirrun","UT","24467");
-		people[8] = new Person("Clayton","Sims",27,"Habso Pike","Jawazgib","WY","54649");
-		people[9] = new Person("Kenneth","Vargas",58,"Diig Terrace","Fazraza","WA","99463");
-		people[10] = new Person("Sally","Watson",20,"Micat Glen","Alolizzo","CO","87846");
-		people[11] = new Person("Aiden","Strickland",42,"Mesa Center","Numillub","NY","39096");
-		people[12] = new Person("Ida","Simmons",42,"Ofti Grove","Funpokru","WA","26564");
-		people[13] = new Person("Florence","Wood",21,"Vapnu Square","Omojobo","NJ","11501");
-		people[14] = new Person("Catherine","Crawford",54,"Nedub Parkway","Pagusnal","IL","94825");
-		people[15] = new Person("Hulda","Wells",63,"Lolfi Road","Awtove","NJ","49980");
-		people[16] = new Person("Lewis","Wade",56,"Padreg Mill","Parzacul","FL","02855");
-		people[17] = new Person("Abbie","Stephens",23,"Wiru Mill","Vokalato","SC","28569");
-		people[18] = new Person("Tommy","Watts",18,"Wovpon Extension","Gedarguc","MD","01979");
-		people[19] = new Person("Jack","Brown",64,"Cetbaj Pike","Pujjapes","WI","19207");
-		people[20] = new Person("Nicholas","Henry",61,"Hacede Ridge","Sonudac","FL","59054");
-		people[21] = new Person("Claudia","Allison",33,"Gesri Trail","Mubhidmo","UT","29629");
-		people[22] = new Person("Theresa","Harrison",49,"Ibihig Turnpike","Welopzi","CA","59460"); 
-		people[23] = new Person("Blake","Roberson",64,"Docuk Point","Bifira","SC","90570");
-		people[24] = new Person("Theodore","Gardner",20,"Jada Key","Bejimu","ID","29394");
+		//people[0] = new Person("Dustin","Cobb", 61,"Pepkor Park", "Zuhhotseb", "NM", "37990");
+		
 		
 		//Person[] people1 = people.clone();
 	//	selectionSort(people1);
@@ -125,6 +108,16 @@ public class Main {
 					j--;
 				}
 			people[j+1] = key;
+			}
+		}
+		
+		void readAllLines() throws IOException
+		{
+			List<Person[]> people;
+			Files.readAllLines(Paths.get("People.csv"));
+			for(Person[] : people)
+			{
+				
 			}
 		}
 }
